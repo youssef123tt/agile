d762d8cb393da941ed7b129f3529e9bda32daf31
“youssef123tt” <125928226+youssef123tt@users.noreply.github.com>
2024-11-01 21:46:13 +0200

    added new files

--- 21P0130_Youssefmahmoud.cpp
+++ 21P0130_Youssefmahmoud.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 
-int main(){
+int summation(int n) {
+    int sum = 0;
+    for (int i = 1; i <= n; ++i) {
+        sum += i;
+    }
+    return sum;
+}
 
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
 
-    cout<<"test";
+    int result = summation(n);
+    cout << "The summation from 1 to " << n << " is: " << result << endl;
+
+    return 0;
 }
